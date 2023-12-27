48de193a1a137706ce40a2055705602e8d3c9b98
Szymon Półtorak <szymonpotorak@gmail.com>
2023-12-27 15:52:18 +0100

    Added some basic home components

--- bank-frontend/src/app/pages/home/home.module.ts
+++ bank-frontend/src/app/pages/home/home.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from "./home.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+    declarations: [
+        HomeComponent
+    ],
+    imports: [
+        CommonModule,
+        HomeRoutingModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+    ]
 })
-export class HomeModule { }
+export class HomeModule {
+}
